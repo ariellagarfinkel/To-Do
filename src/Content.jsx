c16b102dadcb5d81edd5d395fdc125d1cd9de00d
Ariella Garfinkel <ariellagarfinkel@Ariellas-MacBook-Pro.local>
2024-03-14 10:07:04 -0500

    mapped thru the todo titles  and displayed them in a list

--- src/Content.jsx
+++ src/Content.jsx
@@ -28,55 +28,31 @@ handleAllToDo();
 }, [])
 
  return (
-    //   <div>
-    //     <div>
-    //       <h1>Display the data</h1>
-    //     </div>
-    //     <div>
-    //       {todo.comment}
-    //       {todo.title}
-    //       {todo.completed ? "completed" : "not completed"}
-    //     </div>
-    //     <div>
-    //      <h1>List of ToDos</h1>
-    //     </div>
-    //     <div>
-    //       {alltodo.map((todo) =>(
-    //         <div key = {todo.title}>
-    //           <p>{todo.comment}</p>
-    //           <p>{todo.title}</p>
-    //           <p>{todo.completed ? "completed" : "not completed" }</p>
-    //         </div>
-    //         ))}
-    //     </div>
-    //   </div>
-  
     <>
-  <h1>List of To Dos</h1>
-    {/* {alltodo.map((todo) =>(
-    <div key = {todo.title}>
-    ))} */}
-  <ul class="list-group">
-  <li class="list-group-item">
-    <input class="form-check-input me-1" type="checkbox" value="" id="firstCheckbox"/>
-    <label class="form-check-label" for="firstCheckbox">{todo.title}</label>
-  </li>
-  <li class="list-group-item">
-    <input class="form-check-input me-1" type="checkbox" value="" id="secondCheckbox"/>
-    <label class="form-check-label" for="secondCheckbox">{todo.title}</label>
-  </li>
-  <li class="list-group-item">
-    <input class="form-check-input me-1" type="checkbox" value="" id="thirdCheckbox"/>
-    <label class="form-check-label" for="thirdCheckbox">{todo.title}</label>
-  </li>
-</ul>
- </> 
+      <div>
+        <h1>List of To Dos</h1>
+      </div>
+        {alltodo.map((todo) =>(
+        <div key = {todo.title}>
+          <ul className="list-group">
+            <li className="list-group-item">
+              <input className="form-check-input me-1" type="checkbox" value="" id="firstCheckbox"/>
+              <label className="form-check-label" for="firstCheckbox">{todo.title}</label>
+            </li>
+            {/* <li className="list-group-item">
+              <input className="form-check-input me-1" type="checkbox" value="" id="secondCheckbox"/>
+              <label className="form-check-label" for="secondCheckbox">{todo.title}</label>
+            </li>
+            <li className="list-group-item">
+              <input className="form-check-input me-1" type="checkbox" value="" id="thirdCheckbox"/>
+              <label className="form-check-label" for="thirdCheckbox">{todo.title}</label>
+            </li> */}
+          </ul>
+      </div>
+      ))}
+    </> 
  ) 
 }
 
-
-
-
-
-
 export default Content;
+
