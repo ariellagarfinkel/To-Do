96c14ea27482de147ac14ce31bf7e41752d9b99a
Ariella Garfinkel <ariellagarfinkel@Ariellas-MacBook-Pro.local>
2024-03-20 10:41:06 -0500

    add a css file for calendar component

--- src/CalendarComponent.jsx
+++ src/CalendarComponent.jsx
@@ -4,13 +4,17 @@ import { useState } from "react";
 
 function CalendarComponent() {
     const [value, onChange] = useState(new Date());
-
     return (
-        <div>
-            <DatePicker onChange={onChange} value ={value} />
+        <div className="container-container">
+            <DatePicker onChange={onChange} 
+                        value ={value}
+                        calendarIcon={null}
+                        clearIcong={null}
+                        minDate={new Date()}
+                        />
         </div>
-    )
-}
+    );
+    };
 
 
 export default CalendarComponent;
