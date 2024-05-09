73ef68da2a4acbdb5daed154a7f4052ec79fae8a
Ariella Garfinkel <ariellagarfinkel@Ariellas-MacBook-Pro.local>
2024-05-09 11:22:56 -0500

    try to create an object

--- src/CreateToDo.jsx
+++ src/CreateToDo.jsx
@@ -7,14 +7,28 @@ import axios from "axios";
 function CreateToDo(props) {
 
   const [todo, setToDo] = useState({});
+  const [title, setTitle] = useState("");
+  const [dueDate, setDueDate] = useState("");
+  const [comment, setComment] = useState("");
 
   const handleSubmit = (event) => {
      event.preventDefault();
-     const params = new FormData(event.target);
-     handleCreateToDo(params, () => event.target.reset());
+    //  const params = new FormData(event.target);
+     const params = {
+      title: title,
+      dueDate: dueDate,
+      comment: comment
+     };
+     console.log("submit to do", params);
+    //  handleCreateToDo(params, () => event.target.reset());
+     handleCreateToDo(params, () => {
+      setTitle("");
+      setDueDate(new Date());
+      setComment("");
+     });
      console.log("To Do created successfully ");
     }
-
+// create an object to send back to the server. post data to the server. 
 
     const handleCreateToDo = (params, successCallback) => {
       console.log("handleCreateToDo", params);
@@ -26,34 +40,23 @@ function CreateToDo(props) {
 
     return(
       <form onSubmit={handleSubmit}> 
-        {/* <div>
-              <label className="form-check-label" htmlFor="firstCheckbox"> Title </label>
-              </div> */}
               <div className="form-floating mb-3">
                 <input type="title" className="form-control" id="floatingInput" placeholder="Title"/>
                 <label for="floatingInput">Title</label>
               </div>
-              {/* <div>
-                <strong>Due Date:</strong> <label className="form-check-label" htmlFor="firstCheckbox"></label>
-              </div> */}
                 <div className="btn-group">
                   <button type="button" className="btn btn-secondary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false"> Due Date </button>
                   <ul className="dropdown-menu">
                     <li><a className="dropdown-item" href="#"><DatePicker /></a></li>
                   </ul>
                 </div>
-              {/* <div>
-               <label className="form-check-label" htmlFor="firstCheckbox"> Comment</label>
-              </div> */}
               <div className="form-floating mb-3">
                 <input type="comment" className="form-control" id="floatingInput" placeholder="comment"/>
                 <label for="floatingInput">Comment</label>
               </div>
-              {/* <input className="form-check-input me-1" type="checkbox" value="" id="firstCheckbox"/> */}
               <button 
-                type="button" 
+                type="submit" 
                 className="btn btn-outline-secondary" onSubmit={handleCreateToDo}>
-                  console.log("handleCreateToDo")
                 Submit
               </button>
       </form> 
